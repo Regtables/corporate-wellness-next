8a5e64dc6306758fb5b6168d3d45155c8a2b12af
Reghardt Pienaar <reghardt7@gmail.com>
2024-09-18 14:44:41 +0200

    show more & more

--- components/HiddenContent.tsx
+++ components/HiddenContent.tsx
@@ -3,14 +3,14 @@ import React, { FC } from 'react'
 interface HiddenContentProps {
   className: string,
   toggle: boolean,
-  setToggle: () => void
+  setToggle: (toggle: boolean) => void
 }
 
 const HiddenContent:FC<HiddenContentProps> = ({ className, toggle, setToggle }) => {
   return (
     <div className='bg-white w-full h-full relative flex justify-center items-start'>
       <div className='bg-white h-[100px] blur-[40px] abslote top-0 z-10' />
-      <button className='bg-black text-white px-6 py-2 rounded-xl mt-8'>
+      <button className='bg-black text-white px-6 py-2 rounded-xl mt-8' onClick={() => setToggle(true)}>
         Show more
       </button>
     </div>
--- components/TestimonialsSection.tsx
+++ components/TestimonialsSection.tsx
@@ -1,51 +1,58 @@
-'use client'
+"use client";
 
-import { SanityTestimonialsSection } from '@/lib/sanity/types'
-import React, { FC, useState } from 'react'
-import TestimonialCard from './cards/TestimonialCard'
-import Icon from './logos/Icon'
-import { useMediaQuery } from 'react-responsive'
-import HiddenContent from './HiddenContent'
-import CardMotionWrapper from './CardMotionWrapper'
-import AnimatedTitle from './motion/AnimatedTitle'
+import { SanityTestimonialsSection } from "@/lib/sanity/types";
+import React, { FC, useState } from "react";
+import TestimonialCard from "./cards/TestimonialCard";
+import Icon from "./logos/Icon";
+import { useMediaQuery } from "react-responsive";
+import CardMotionWrapper from "./CardMotionWrapper";
+import AnimatedTitle from "./motion/AnimatedTitle";
+import BasicButton from "./buttons/BasicButton";
 
+const TestimonialsSection: FC<SanityTestimonialsSection> = ({
+  heading,
+  testimonials,
+}) => {
+  const [showMore, setShowMore] = useState(false);
+  const isMobile = useMediaQuery({ maxWidth: "676px" });
 
-const TestimonialsSection:FC<SanityTestimonialsSection> = ({ heading, testimonials }) => {
-  const [toggleMore, setToggleMore] = useState(false)
-  const isMobile = useMediaQuery({ maxWidth: '676px' })
+  const visibleTestimonials = isMobile && !showMore ? testimonials.slice(0, 3) : testimonials;
 
   return (
-    <section className='section-padding py-32 relative max-h-[200vh] overflow-hidden' id = 'testimonials'>
+    <section className="section-padding py-32 relative" id="testimonials">
+      <AnimatedTitle text={heading} className="heading" />
 
-      <AnimatedTitle text= {heading} className='heading' />
-      
-      <div className='grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-8 pt-10 relative' style = {toggleMore ? { maxHeight: 'unset' } : { maxHeight: '100vh' }}>
-        {testimonials.map((testimonial, i) => (
-          <CardMotionWrapper delay={i*0.1} duration={1}>
-            <TestimonialCard 
-              name= {testimonial.name}
-              country= {testimonial.country}
-              text= {testimonial.text}
+      <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-8 pt-10 relative">
+        {visibleTestimonials.map((testimonial, i) => (
+          <CardMotionWrapper key={i} delay={i * 0.1} duration={1}>
+            <TestimonialCard
+              name={testimonial.name}
+              country={testimonial.country}
+              text={testimonial.text}
             />
           </CardMotionWrapper>
         ))}
-
-        {!toggleMore && isMobile && (
-          <div className='absolute h-[30%] z-20 -bottom-[40px] -start-1 end-3 w-[102%]'>
-            <HiddenContent 
-              toggle = {toggleMore} 
-              //@ts-ignore
-              setToggle={setToggleMore} 
-            />
-          </div>
-        )}
       </div>
-      
-      <div className='lg:block hidden absolute bottom-[0px] -start-[0px]'>
-        <Icon path={{src: '/baobab-sml.png'}} className="h-[330px] w-[157px]"/>
+
+      {isMobile && testimonials.length > 3 && (
+        <div className="flex justify-center mt-8">
+          <BasicButton
+            text={showMore ? "Show less" : "Show more"}
+            bgColor="var(--color-black)"
+            color="white"
+            onClick={() => setShowMore(!showMore)}
+          />
+        </div>
+      )}
+
+      <div className="lg:block hidden absolute bottom-[0px] -start-[0px]">
+        <Icon
+          path={{ src: "/baobab-sml.png" }}
+          className="h-[330px] w-[157px]"
+        />
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default TestimonialsSection
+export default TestimonialsSection;
