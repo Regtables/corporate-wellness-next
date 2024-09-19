44732d0e2290a5eed4901cee533d2964da1dd492
Reghardt Pienaar <reghardt7@gmail.com>
2024-09-19 09:43:05 +0200

    loading func

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import type { Metadata } from "next";
-
 import "./globals.scss";
 import { combineFontClasses, libreBaskerville, nunitoSans } from "@/lib/fonts";
 import { ParallaxProvider } from "react-scroll-parallax";
@@ -13,11 +12,44 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const [isFirstLoad, setIsFirstLoad] = useState(true)
+  const [isFirstLoad, setIsFirstLoad] = useState(true);
+
+  const handleLoad = () => {
+    if (typeof window !== 'undefined') {
+      const isRecentString = localStorage.getItem('isRecent');
+      if (isRecentString) {
+        const isRecent = JSON.parse(isRecentString);
+        if (isRecent.date) {
+          const lastVisit = new Date(isRecent.date);
+          const now = new Date();
+          const hoursSinceLastVisit = (now.getTime() - lastVisit.getTime()) / (1000 * 60 * 60);
+          
+          if (hoursSinceLastVisit < 1) {
+            // Less than 1 hour since last visit, don't show loading screen
+            setIsFirstLoad(false);
+          } else {
+            // More than 1 hour, update the time and show loading screen
+            localStorage.setItem('isRecent', JSON.stringify({ date: now.toISOString() }));
+            setIsFirstLoad(true);
+          }
+        } else {
+          // Invalid date in localStorage, set current time and show loading screen
+          const now = new Date();
+          localStorage.setItem('isRecent', JSON.stringify({ date: now.toISOString() }));
+          setIsFirstLoad(true);
+        }
+      } else {
+        // No recent visit recorded, set current time and show loading screen
+        const now = new Date();
+        localStorage.setItem('isRecent', JSON.stringify({ date: now.toISOString() }));
+        setIsFirstLoad(true);
+      }
+    }
+  };
 
   useEffect(() => {
-    setIsFirstLoad(false)
-  }, [])
+    handleLoad();
+  }, []);
   
   return (
     <html
@@ -29,7 +61,7 @@ export default function RootLayout({
     >
       <body>
         <ParallaxProvider>
-          {!isFirstLoad && <LoadingScreen />}
+          {isFirstLoad && <LoadingScreen />}
           {children}
         </ParallaxProvider>
       </body>
